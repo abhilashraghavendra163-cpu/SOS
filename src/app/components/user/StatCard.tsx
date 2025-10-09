d199b43c4960ef8bd9d42b9741e8998609b68ca7
ABHILASH RAGHAVENDRA ADUGANI VANJARI <abhilashraghavendra163@gmail.com>
2025-10-09 18:29:23 +0000

    Try fixing this error: `Console Error: Only plain objects can be passed

--- src/app/components/user/StatCard.tsx
+++ src/app/components/user/StatCard.tsx
@@ -7,21 +7,23 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import type { LucideIcon } from "lucide-react";
+import * as Icons from "lucide-react";
 
 type StatCardProps = {
   title: string;
   value: number | string;
-  icon: LucideIcon;
+  icon: keyof typeof Icons;
   color?: string;
 };
 
-export function StatCard({ title, value, icon: Icon, color }: StatCardProps) {
+export function StatCard({ title, value, icon, color }: StatCardProps) {
+  const Icon = Icons[icon] as Icons.LucideIcon;
+
   return (
     <Card className="transform transition-transform duration-300 hover:scale-[1.05] hover:shadow-lg">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium">{title}</CardTitle>
-        <Icon className={cn("h-5 w-5 text-muted-foreground", color)} />
+        {Icon && <Icon className={cn("h-5 w-5 text-muted-foreground", color)} />}
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">{value}</div>
