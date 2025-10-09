d9ca1c3ab72c847dc2aadef17893f6386d78a442
ABHILASH RAGHAVENDRA ADUGANI VANJARI <abhilashraghavendra163@gmail.com>
2025-10-09 18:54:19 +0000

    need responsive

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ export default function UserDashboardPage() {
       <div className="lg:col-span-1 flex flex-col gap-6">
         <AttendanceCard />
         <MyPayrollCard />
-        <div className="grid grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           <StatCard
             title="Present Days"
             value={presentDays}
