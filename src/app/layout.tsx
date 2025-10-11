ac888a38548c0a50f0ae4ffebcf1083495b13f7b
ABHILASH RAGHAVENDRA ADUGANI VANJARI <abhilashraghavendra163@gmail.com>
2025-10-11 17:35:27 +0000

    change ui and ux to google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { Inter as FontSans } from "next/font/google"
+import { Roboto as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from './components/ThemeProvider';
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  weight: ["400", "500", "700"],
 })
 
 export const metadata: Metadata = {
@@ -25,7 +26,7 @@ export default function RootLayout({
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
