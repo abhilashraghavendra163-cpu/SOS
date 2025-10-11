427d5db9dff547e2e5a09e836f1bbb3eb1180438
ABHILASH RAGHAVENDRA ADUGANI VANJARI <abhilashraghavendra163@gmail.com>
2025-10-11 17:34:40 +0000

    change ui to normal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { Roboto as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from './components/ThemeProvider';
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-  weight: ["400", "500", "700"],
 })
 
 export const metadata: Metadata = {
