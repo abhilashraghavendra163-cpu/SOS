a341263c2ee1d841c9d69440f098f4bee486f4c3
ABHILASH RAGHAVENDRA ADUGANI VANJARI <abhilashraghavendra163@gmail.com>
2025-11-15 13:22:13 +0000

    ⚠ Cross origin request detected from 3000-firebase-studio-1760033313233.

--- next.config.js
+++ next.config.js
@@ -32,7 +32,12 @@ const nextConfig = {
   },
    env: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: "AIzaSyCg3Kplz0Ne1x8RQK2-9wzZFm1F1bRf5T4",
-  }
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      "*.cloudworkstations.dev"
+    ],
+  },
 };
 
 module.exports = nextConfig;
